3485d388a6319eb1624173d64f41e10ecc87f953
Hongbin-Liu <hongbin.liu@seattlecolleges.edu>
2021-05-04 17:29:42 -0700

    Assignment 4 Models

--- Club/admin.py
+++ Club/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Meeting, Minute, Resource, Event
 
 # Register your models here.
+admin.site.register(Meeting)
+admin.site.register(Minute)
+admin.site.register(Resource)
+admin.site.register(Event)
