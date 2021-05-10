ba59ab74e8e1124ba069a709e765caa49562c413
Hongbin-Liu <hongbin.liu@seattlecolleges.edu>
2021-05-09 20:08:06 -0700

    Assignment 5

--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Resource, Meeting
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def getResource(request):
+    res = Resource.objects.all()
+    return render(request, 'club/Resource.html', {"Resource": res})
