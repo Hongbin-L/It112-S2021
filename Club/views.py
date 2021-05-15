b637d893dc98b8ce1a6317c53874835e4df0e798
Hongbin-Liu <hongbin.liu@seattlecolleges.edu>
2021-05-15 13:25:56 -0700

    Assignment week 6

--- Club/views.py
+++ Club/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Resource, Meeting
 
 # Create your views here.
@@ -8,3 +8,11 @@ def index(request):
 def getResource(request):
     res = Resource.objects.all()
     return render(request, 'club/Resource.html', {"Resource": res})
+
+def getMeeting(request):
+    met = Meeting.objects.values('id', 'title')
+    return render(request, 'club/Meeting.html', {"Meetings": met})
+
+def meetingDetails(request, id):
+    details = get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/meetingDetails.html', {"details": details})
